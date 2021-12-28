d2b771db81b84bcdc292dd34276d78168233c70f
Adrian Olosutean <adi.olosutean@gmail.com>
2021-12-28 16:16:23 +0100

    update licenses

--- src/main/scala/za/co/absa/spark/commons/config/PathWithFs.scala
+++ src/main/scala/za/co/absa/spark/commons/config/PathWithFs.scala
@@ -1,9 +1,10 @@
 /*
- * Copyright 2018 ABSA Group Limited
+ * Copyright 2021 ABSA Group Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -12,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 
 package za.co.absa.spark.commons.config
 
