dd2aba2cd428fb363a89ea6b2e199145193b6d57
David Benedeki <14905969+benedeki@users.noreply.github.com>
2022-11-10 19:41:05 +0100

    #67: Make it easy to avoid repeated UDF registration (#68)
    * `OncePerSparkSession` class implementation
    * increased version of used scalatest from 3.1.0 to 3.2.2
    * added dependency on mockito-scala 1.17.12

---
+++ spark-commons/src/main/scala/za/co/absa/spark/commons/OncePerSparkSession.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.spark.commons
+
+import org.apache.spark.sql.SparkSession
+
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ * Abstract class to help attach/register UDFs and similar object only once to a spark session.
+ * This is done by using the companion object's registry [[ConcurrentHashMap]] that holds already
+ * instantiated classes thus not running the method [[register]] again on them.
+ *
+ * Usage: extend this abstract class and implement the method [[register]]. On initialization the
+ * [[register]] method gets called by the [[registerMe]] method if the class + spark session
+ * combination is unique. If it is not unique [[register]] will not get called again.
+ * This way we ensure only single registration per spark session.
+ *
+ * @param sparkToRegisterTo Spark session to which we wish to attach objects
+ */
+abstract class OncePerSparkSession()(implicit sparkToRegisterTo: SparkSession) extends Serializable {
+
+  protected def register(implicit spark: SparkSession): Unit
+
+  OncePerSparkSession.registerMe(this, sparkToRegisterTo)
+}
+
+object OncePerSparkSession {
+  private[this] type Key = (Int, String)
+
+  private[this] val registry = new ConcurrentHashMap[Key, Unit]
+
+  private[this] def makeKey(library: OncePerSparkSession, spark: SparkSession): Key = {
+    (
+      spark.hashCode(),  //using hash as sufficiently unique and allowing garbage collection
+      library.getClass.getName
+    )
+  }
+
+  private def registerMe(library: OncePerSparkSession, spark: SparkSession): Unit = {
+    Option(registry.putIfAbsent(makeKey(library, spark), Unit))
+      .getOrElse(library.register(spark))
+  }
+}
