63e7e3db2f7c2e9332d026a0384c42dac52ea44c
David Benedeki <benedeki@volny.cz>
2023-03-20 08:43:45 +0100

    * Added UTs for `ColumnOrValue` * Fixed few minor things discovered by the UTs

---
+++ spark-commons/src/test/scala/za/co/absa/spark/commons/errorhandling/types/ColumnOrValueForm.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.spark.commons.errorhandling.types
+
+import org.apache.spark.sql.Column
+import org.scalatest.Assertions
+
+case class ColumnOrValueForm[T] (
+                                 column: Column,
+                                 isColumn: Boolean,
+                                 isValue: Boolean,
+                                 columnNames: Set[String],
+                                 value: Option[T]
+                               ) extends Assertions {
+  def assertTo(columnOrValue: ColumnOrValue[T]): Unit ={
+    assert(column == columnOrValue.column)
+    assert(isColumn == columnOrValue.isColumn)
+    assert(isValue == columnOrValue.isValue)
+    assert(columnNames == columnOrValue.columnNames)
+    assert(value == columnOrValue.getValue)
+  }
+
+}
