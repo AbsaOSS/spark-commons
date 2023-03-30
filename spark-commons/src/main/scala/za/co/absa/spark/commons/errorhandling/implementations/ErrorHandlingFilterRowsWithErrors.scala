2152467c8a715bc65c4a3370723edad50b65e425
AB019TC <tebalelo.sekhula@absa.africa>
2023-03-30 16:10:13 +0200

    Fixes #93

--- spark-commons/src/main/scala/za/co/absa/spark/commons/errorhandling/implementations/ErrorHandlingFilterRowsWithErrors.scala
+++ spark-commons/src/main/scala/za/co/absa/spark/commons/errorhandling/implementations/ErrorHandlingFilterRowsWithErrors.scala
@@ -6,14 +6,26 @@ import za.co.absa.spark.commons.errorhandling.{ErrorMessageSubmit}
 import za.co.absa.spark.commons.errorhandling.partials.ErrorHandlingCommon
 import za.co.absa.spark.commons.errorhandling.types.ErrorColumn
 
+/**
+ *
+ */
+
 object ErrorHandlingFilterRowsWithErrors extends ErrorHandlingCommon {
   override def aggregateErrorColumns(dataFrame: DataFrame)(errCols: ErrorColumn*): DataFrame = {
+    register(dataFrame.sparkSession)
+    doTheAggregation(dataFrame, errCols.map(_.column): _*)
+  }
+
+  override protected def evaluate(errorMessageSubmit: ErrorMessageSubmit): Column = {
+//    val colCheck: Array[Boolean] = errorMessageSubmit.fill(errorColumn =>false).toArray()
+    val columnCheck: Array[Boolean] = Array.fill(errorMessageSubmit.errColsAndValues)(false)
+    columnCheck
+  }
+
+  override protected def doTheAggregation(dataFrame: DataFrame, errCols: Column*): DataFrame = {
     val aggregatedDF = dataFrame.groupBy("errCode")
       .agg(coalesce(collect_list("errCols")) as "AggregatedError")
     aggregatedDF.filter(!col("AggregatedError"))
   }
 
-  override protected def evaluate(errorMessageSubmit: ErrorMessageSubmit): Column = ???
-
-  override protected def doTheAggregation(dataFrame: DataFrame, errCols: Column*): DataFrame = ???
 }
