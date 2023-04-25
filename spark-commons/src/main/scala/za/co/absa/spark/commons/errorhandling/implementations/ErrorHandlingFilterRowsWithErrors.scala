dab6738c7cd953afe20df26be83c24d441dfbb55
TebaleloS <107194332+TebaleloS@users.noreply.github.com>
2023-04-25 08:15:06 +0200

    Update spark-commons/src/main/scala/za/co/absa/spark/commons/errorhandling/implementations/ErrorHandlingFilterRowsWithErrors.scala
    Co-authored-by: David Benedeki <14905969+benedeki@users.noreply.github.com>

--- spark-commons/src/main/scala/za/co/absa/spark/commons/errorhandling/implementations/ErrorHandlingFilterRowsWithErrors.scala
+++ spark-commons/src/main/scala/za/co/absa/spark/commons/errorhandling/implementations/ErrorHandlingFilterRowsWithErrors.scala
@@ -23,7 +23,7 @@ object ErrorHandlingFilterRowsWithErrors extends ErrorHandlingCommon {
 
   /**
    * Creates a column with the error description, in this particular case actually only signals with a boolean flag there was an error in the row.
-   * @param errorMessageSubmit the object that defines the structure of the column
+   * @param errorMessageSubmit - the description of the error
    * @return returns true if the column contains an error
    */
   override protected def evaluate(errorMessageSubmit: ErrorMessageSubmit): Column = {
