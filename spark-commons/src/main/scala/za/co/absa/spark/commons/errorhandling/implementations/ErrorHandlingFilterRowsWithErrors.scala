113a7430733d08097ae7632e684b90bc0004bfd2
AB019TC <tebalelo.sekhula@absa.africa>
2023-07-06 14:46:58 +0200

    Fixes 88

--- spark-commons/src/main/scala/za/co/absa/spark/commons/errorhandling/implementations/ErrorHandlingFilterRowsWithErrors.scala
+++ spark-commons/src/main/scala/za/co/absa/spark/commons/errorhandling/implementations/ErrorHandlingFilterRowsWithErrors.scala
@@ -18,7 +18,7 @@ package za.co.absa.spark.commons.errorhandling.implementations
 
 import org.apache.spark.sql.{Column, DataFrame}
 import org.apache.spark.sql.functions.{coalesce, lit}
-import org.apache.spark.sql.types.{BooleanType, DataType}
+import org.apache.spark.sql.types.{ DataType, NullType}
 import za.co.absa.spark.commons.errorhandling.{ErrorHandling, ErrorMessageSubmit}
 
 /**
@@ -51,7 +51,7 @@ object ErrorHandlingFilterRowsWithErrors extends ErrorHandling {
    * This function provides the information on the structure of single error column
    * @return - `BooleanType`, as all what is needed is a true flag if error was present
    */
-  val errorColumnType: DataType = BooleanType
+  val errorColumnType: DataType = NullType
 
   /**
    * Provides the library some information about how the actual implementation of [[ErrorHandling]] is structured.
